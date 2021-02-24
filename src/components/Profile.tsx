2fa45faf04013c892a08e8097dd689e4619624f1
Glauber Gadelha <glauber.gadelha@cinbesa.com.br>
2021-02-23 22:49:18 -0300

    ultimo push do dia 2

---
+++ src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import styles from "../styles/components/Profile.module.css";
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/glaubergad.png" alt="Glauber Gadelha" />
+      <div>
+        <strong>Glauber Gadelha</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" height="5px" />
+          Level 1
+        </p>
+      </div>
+    </div>
+  );
+}
